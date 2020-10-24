fe6890c6e438dfea83a36efb1370d41f849c9fa8
Brian Hung <brianhung@berkeley.edu>
2020-10-24 15:42:05 -0700

    :sparkles: add cjs module

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  input: './index.js',
+  output: [{
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  }, {
+    file: 'dist/index.es.js',
+    format: 'es',
+    sourcemap: true
+  }],
+}
